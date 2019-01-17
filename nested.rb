103f69c462b5da4cd6b43ea1c3e13a67eb933aa8
Akram Helil <akramhelil@gmail.com>
2019-01-17 13:59:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require.pry
 def hopper
 	programmer_hash =
  		{
@@ -104,10 +104,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
-
+programmer_hash[:alan_kay][:known_for] = alans_new_info
+programmer_hash
 end
-
+binding.pry
 def adding_to_dennis
 	programmer_hash =
  		{
